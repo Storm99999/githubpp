7e46df474a74493f0d21d32e98a5378188089373
newguy <87811650+Storm99999@users.noreply.github.com>
2023-11-09 23:27:09 +0100

    Create injector.js

---
+++ src/injector.js
@@ -0,0 +1,19 @@
+var theme = "Dracula"
+
+function main() {
+    const injectCSS = css => {
+        let x = document.createElement('style');
+        x.type = ('text/css'); // deprecated, but i do not give two shits
+        x.innerText = css;
+        document.head.appendChild(x);
+        return x;
+    };
+
+    let theme_storage = {
+        "Dracula": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/dracula/dracula_main.css"
+    };
+
+    if (theme == "Dracula") { fetch(theme_storage['Dracula']).then(response => response.text()).then(data => injectCSS(data)).catch(error => console.error('Error:', error)); };
+}
+
+main();
