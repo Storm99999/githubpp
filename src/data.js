b0ca72310bc290a62aad78736b83c3f6af417521
newguy <87811650+Storm99999@users.noreply.github.com>
2023-11-11 12:20:57 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,9 @@
+let theme_storage = {
+    "Dracula": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/dracula/dracula_main.css",
+    "TokyoNight": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/tokyo_night/tokyo_night_main.css",
+    "Aura": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/aura/aura_main.css",
+    "Jellybeans": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/jellybeans/jellybeans_main.css",
+    "CustomTestTheme": "https://raw.githubusercontent.com/Storm99999/githubpp/main/tests/theme2.css"
+};
+
+module.exports = theme_storage;
