b0a86250916d02205cd4ec5ffc5d47a0a88c141e
newguy <87811650+Storm99999@users.noreply.github.com>
2023-11-11 12:42:24 +0100

    New and Improved loader

--- ext/githubpp.js
+++ ext/githubpp.js
@@ -12,27 +12,32 @@
 (function() {
     'use strict';
 
-    var theme = "Jellybeans"
+    var theme = "Aura"
 
     function main() {
         const injectCSS = css => {
             let x = document.createElement('style');
-            x.type = ('text/css'); // deprecated, but i do not give two shits
+            x.type = ('text/css'); // deprecated, but I do not give two shits
             x.innerText = css;
             document.head.appendChild(x);
             return x;
         };
 
-        let theme_storage = {
-            "Dracula": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/dracula/dracula_main.css",
-            "TokyoNight": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/tokyo_night/tokyo_night_main.css",
-            "Aura": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/aura/aura_main.css",
-            "Jellybeans": "https://raw.githubusercontent.com/Storm99999/githubpp/main/src/themes/jellybeans/jellybeans_main.css",
-            "CustomTestTheme": "https://raw.githubusercontent.com/Storm99999/githubpp/main/tests/theme2.css"
-        };
-
-        // Injection process
-        fetch(theme_storage[theme]).then(response => response.text()).then(data => injectCSS(data)).catch(error => console.error('Error:', error));
+        // Fetch themes.json dynamically
+        fetch('https://raw.githubusercontent.com/Storm99999/githubpp/main/src/theme_list.json')
+            .then(response => response.json())
+            .then(theme_storage => {
+                // Check if the selected theme exists in the retrieved data
+                if (theme_storage[theme]) {
+                    fetch(theme_storage[theme])
+                        .then(response => response.text())
+                        .then(data => injectCSS(data))
+                        .catch(error => console.error('Error:', error));
+                } else {
+                    console.error('Error: Theme not found in themes.json');
+                }
+            })
+            .catch(error => console.error('Error fetching themes.json:', error));
     }
 
     main();
